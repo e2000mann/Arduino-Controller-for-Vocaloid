2e5d9709faf1138c5eacf8483c9a2299b0ddbfef
e2000mann <e2000mann@outlook.com>
2021-07-17 11:11:24 +0100

    update 17 july

--- code/code.ino
+++ code/code.ino
@@ -7,8 +7,17 @@
 // global variables
 // pitch uses midi numbers (60 = C4)
 const int DEFAULTPITCH = 60;
+
 const float DEFAULTLENGTH = 0.125;
+const float LENGTHS[7] = {0.0625, 0.125, 0.25, 0.5, 1, 2, 4};
+const int CURLENGTHVALUE = 0;
+
 const String DEFAULTSYLLABLE = "a";
+
+String CURRENTSYLLABLE = DEFAULTSYLLABLE;
+float CURRENTLENGTH = DEFAULTLENGTH;
+int CURRENTPITCH = DEFAULTPITCH;
+
 // pins (digital)
 // pitch pins
 const int pitchUp = 0, pitchDown = 1;
@@ -20,9 +29,10 @@ const int submit = 2;
 const int rs = 8, en = 9, d4 = 10, d5= 11, d6= 12, d7= 13;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 // pins (analog)
-int length = A0;
+int lengthValue = A0;
 int vowelValue = A1;
 int consonantValue = A2;
+int contrast = A3;
 
 void setup() {
   // initialise pins as input
@@ -32,50 +42,76 @@ void setup() {
   pinMode(addY, INPUT);
   pinMode(handakuten, INPUT);
   pinMode(dakuten, INPUT);
+  
   Serial.begin(9600); // serial needed to use analog pins
 
+  // initialise lcd
+  analogWrite(contrast, 50);
   lcd.begin(16, 2);
-  lcd.print("hello, world!");
+  lcd.print("current note:");
 }
 
 void loop() {
-  String currentSyllable = DEFAULTSYLLABLE;
-  float currentLength = DEFAULTLENGTH;
-  int currentPitch = DEFAULTPITCH;
-  while (digitalRead(submit) == LOW)
-  { 
-    String newSyllable = checkAnalogPins();
-    if (newSyllable != ""){
-      // don't update syllable if there was invalid input
-      currentSyllable = newSyllable;
-    }
-    currentLength = updateLength();
-    bool pitchUpPress = digitalRead(pitchUp) == HIGH;
-    bool pitchDownPress = digitalRead(pitchDown) == HIGH;
-    if (pitchUpPress || pitchDownPress)
-    {
-      currentPitch = updatePitch(currentPitch, pitchUpPress);
-    }
-    
-    setlcd(currentSyllable, currentLength, currentPitch);
 
+  if (digitalRead(submit) == HIGH){
+    // submit button pressed
+    addNote(CURRENTSYLLABLE, CURRENTLENGTH, CURRENTPITCH);
   }
-  // submit button pressed
-  addNote(currentSyllable, currentLength, currentPitch);
+
+  else {
+    int consonantRead = analogRead(consonantValue);
+    bool updateConst = consonantRead > 160;
+
+    int vowelRead = analogRead(vowelValue);
+    bool updateVowel = vowelRead > 160;
+
+    int lengthRead = analogRead(lengthValue);
+    int range[2] = {CURLENGTHVALUE - 50, CURLENGTHVALUE + 50};
+    bool updateLength = range[0] > lengthRead || range[1] < lengthRead;
+
+    if (updateConst || updateVowel || updateLength){
+      update(updateConst, updateVowel, updateLength);
+    }
+
+    char test[32];
+    snprintf(test, 32, "%d %d %d", consonantRead, vowelRead, lengthRead);
+    Serial.println(test);
+  }
+
+  // while (digitalRead(submit) == LOW)
+  // { 
+  //   String newSyllable = checkAnalogPins();
+  //   bool update = newSyllable != "" || newSyllable != " ";
+  //   if (update){
+  //     // don't update syllable if there was invalid input
+  //     CURRENTSYLLABLE = newSyllable;
+  //   }
+  //   CURRENTLENGTH = updateLength();
+  //   bool pitchUpPress = digitalRead(pitchUp) == HIGH;
+  //   bool pitchDownPress = digitalRead(pitchDown) == HIGH;
+  //   if (pitchUpPress || pitchDownPress)
+  //   {
+  //     CURRENTPITCH = updatePitch(CURRENTPITCH, pitchUpPress);
+  //   }
+    
+    
+
+  // }
 }
 
 void addNote(String syllable, float length, int pitch){
-  // adds note to v5 editor
-  return;
+  // adds note to editor
+  // TODO: how to connect Arduino code to synth v plugin?
+  lcd.clear();
+  lcd.print("note submitted");
+  delay(1000); // so user can see submission note
 }
 
 void setlcd(String syllable, float length, int pitch){
-
-  Serial.print(syllable);
-
   // convert syllable into char array
   char syBuffer[3];
-  syllable.toCharArray(syBuffer, 3);
+  strcpy(syBuffer, syllable.c_str());
+  // syllable.toCharArray(syBuffer, 3);
 
   // convert float into 3dp string (temp)
   char lenBuffer[10];
@@ -84,101 +120,114 @@ void setlcd(String syllable, float length, int pitch){
   char output[32];
   snprintf(output, 32, "%s %s %d", syBuffer, lenBuffer, pitch);
 
+  lcd.clear();
+  lcd.print("current note:");
   lcd.setCursor(0, 1);
   lcd.print(output);
 }
 
+void update(bool updateConst, bool updateVowel, bool updateLength){
+
+  setlcd(CURRENTSYLLABLE, CURRENTLENGTH, CURRENTPITCH);
+}
+
 // TODO: is a string a good output?
-String checkAnalogPins(){
-  String output = "";
-  // checks pins
-  char consonant = getConsonant();
-  delay(100); // for stability
-  char vowel = getVowel();
-  delay(100);
-  if (consonant != 'n' && vowel == ' '){
-    // will have to break
-    return "";
-  }
-  if (consonant == 'h' && vowel == 'u'){
-    consonant = 'f';
-  }
-  // TODO: what vowels don't work with y?
-  if (digitalRead(addY) == HIGH){
-    output = consonant + "y" + vowel;
-  } else {
-    output = consonant + vowel;
-  }
-  return output;
-}
+// String checkAnalogPins(){
+//   String output = "";
+//   // checks pins
+//   char consonant = getConsonant();
+//   delay(100); // for stability
+//   char vowel = getVowel();
+//   delay(100);
+//   if (consonant != 'n' && vowel == ' '){
+//     // will have to break
+//     return "";
+//   }
+//   if (consonant == 'h' && vowel == 'u'){
+//     consonant = 'f';
+//   }
 
-int updatePitch(int curPitch, bool pitchUp){
-  if (pitchUp){
-    // pitch up
-    curPitch += 1;
-  } else {
-    // pitch down
-    curPitch -= 1;
-  }
-  return curPitch;
-}
+//   output.concat(consonant);
+//   // TODO: what vowels don't work with y?
+//   if (digitalRead(addY) == HIGH){
+//     output.concat("y");
+//   }
+//   output.concat(vowel);
 
-float updateLength(){
-  // TODO: how to update length
-  int temp = analogRead(length);
-  return 0.125;
-}
+//   return output;
+// }
 
-// TODO: (here & on getVowel) check values & make switch statements
-char getConsonant(){
-  int temp = analogRead(consonantValue);
-  Serial.println(temp);
-  if (temp < 100){ return ' '; } // no consonant
-  if (temp < 150){
-    // k or g (depends on dakuten)
-    if (digitalRead(dakuten) == HIGH){
-      return 'g';
-    }
-    return 'k';
-  }
-  if (temp < 250){
-    // s or z
-    if (digitalRead(dakuten) == HIGH){
-      return 'z';
-    }
-    return 's';
-  }
-  if (temp < 350){
-    // t or d
-    if (digitalRead(dakuten) == HIGH){
-      return 'd';
-    }
-    return 't';
-  }
-  if (temp < 450){ return 'n'; } // always n
-  if (temp < 550){
-    //h, b or p (or f if f+u, need to work out how that'll work)
-    if (digitalRead(dakuten) == HIGH){
-      return 'b';
-    }
-    if (digitalRead(handakuten) == HIGH){
-      return 'p';
-    }
-    return 'h';
-  }
-  if (temp < 650) { return 'm'; } // always m
-  if (temp < 750) { return 'y'; } // always y
-  if (temp < 850) { return 'r'; } // always r
-  else { return 'w'; } // always w
-}
+// int updatePitch(int curPitch, bool pitchUp){
+//   if (pitchUp){
+//     // pitch up
+//     curPitch += 1;
+//   } else {
+//     // pitch down
+//     curPitch -= 1;
+//   }
+//   return curPitch;
+// }
 
-char getVowel(){
-  int temp = analogRead(vowelValue);
-  Serial.println(temp);
-  if (temp < 100){ return 'a'; }
-  if (temp < 150){ return 'i'; }
-  if (temp < 250){ return 'u'; }
-  if (temp < 350){ return 'e'; }
-  if (temp < 450){ return 'o'; }
-  else { return ' '; }
-}
+// float updateLength(){
+//   // TODO: how to update length
+//   int temp = analogRead(length);
+//   if (temp < 7000){
+//     temp = 7000;
+//   }
+//   int index = temp / 1000;
+//   return LENGTHS[index];
+// }
+
+// // TODO: (here & on getVowel) check values & make switch statements
+// char getConsonant(){
+//   int temp = analogRead(consonantValue);
+//   // Serial.println(temp);
+//   if (temp < 30000){ return ' '; } // no consonant
+//   if (temp < 250000){
+//     // k or g (depends on dakuten)
+//     if (digitalRead(dakuten) == HIGH){
+//       return 'g';
+//     }
+//     return 'k';
+//   }
+//   if (temp < 250){
+//     // s or z
+//     if (digitalRead(dakuten) == HIGH){
+//       return 'z';
+//     }
+//     return 's';
+//   }
+//   if (temp < 350){
+//     // t or d
+//     if (digitalRead(dakuten) == HIGH){
+//       return 'd';
+//     }
+//     return 't';
+//   }
+//   if (temp < 450){ return 'n'; } // always n
+//   if (temp < 550){
+//     //h, b or p (or f if f+u, need to work out how that'll work)
+//     if (digitalRead(dakuten) == HIGH){
+//       return 'b';
+//     }
+//     if (digitalRead(handakuten) == HIGH){
+//       return 'p';
+//     }
+//     return 'h';
+//   }
+//   if (temp < 650) { return 'm'; } // always m
+//   if (temp < 750) { return 'y'; } // always y
+//   if (temp < 850) { return 'r'; } // always r
+//   else { return 'w'; } // always w
+// }
+
+// char getVowel(){
+//   int temp = analogRead(vowelValue);
+//   // Serial.println(temp);
+//   if (temp < 100){ return 'a'; }
+//   if (temp < 150){ return 'i'; }
+//   if (temp < 200){ return 'u'; }
+//   if (temp < 300){ return 'e'; }
+//   if (temp < 450){ return 'o'; }
+//   else { return ' '; }
+// }
